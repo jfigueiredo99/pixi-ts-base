b6b97d8568070d0fc4a21dcdf8869d5abcc3d12f
Miguel Teixeira <miguel.teixeira@ctw.bmwgroup.com>
2023-02-08 12:02:11 +0000

    With dirt tiles

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,12 @@ const app = new PIXI.Application({
 // IMPORT ASSETS - THEY MUST BE AT "DIST/IMAGES"
 app.loader.baseUrl = "images";
 app.loader.add("blank", "blank.png");
-app.loader.add("down", "down.png");
-app.loader.add("left_right", "left_right.png");
-app.loader.add("left", "left.png");
-app.loader.add("right", "right.png");
-app.loader.add("up_down", "up_down.png");
-app.loader.add("up", "up.png");
+app.loader.add("down", "down_dirt.png");
+app.loader.add("left_right", "left_right_dirt.png");
+app.loader.add("left", "left_dirt.png");
+app.loader.add("right", "right_dirt.png");
+app.loader.add("up_down", "up_down_dirt.png");
+app.loader.add("up", "up_dirt.png");
 // app.loader.add("tile", "tile.png");
 // app.loader.add("dirt", "dirt.png");
 // app.loader.add("path", "path.png");
